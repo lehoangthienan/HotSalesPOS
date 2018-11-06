16399a1ee2d48531d57c3bf0cfc21f16f7e41091
An Le <lehoangthienan@gmail.com>
2018-11-06 20:56:32 +0700

    Add logic of api create order

--- api/src/controllers/express/api/order/create.js
+++ api/src/controllers/express/api/order/create.js
@@ -1,20 +1,15 @@
 var config = require('../../../../config');
 var response_express = require(config.library_dir+'/response').response_express;
 var lib_common = require(config.library_dir+'/common');
-var lib_password = require(config.library_dir+'/password');
-var User = require(config.models_dir + '/mongo/user');
+var Order = require(config.models_dir + '/mongo/order');
 
 module.exports = (req, res)=>{
-    let miss=lib_common.checkMissParams(res, req.body, ["user"])
+    let miss=lib_common.checkMissParams(res, req.body, ["order"])
     if (miss) return
 
-    lib_password.cryptPassword(req.body.user.password)
-    .then(passwordHash=>{
-        req.body.user.password = passwordHash
-        return User.create(req.body.user)
-    })
-    .then(user=>{
-        response_express.success(res, user)
+    Order.create(req.body.order)
+    .then(order=>{
+        response_express.success(res, order)
     })
     .catch(err=>response_express.exception(res, err.message))
 }
