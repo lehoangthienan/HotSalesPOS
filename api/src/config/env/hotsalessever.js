32aed020ffe38a7e8ee7b3fcb9685749e619479d
An Le <lehoangthienan@gmail.com>
2018-11-19 09:02:16 +0700

    Set up sever for api

---
+++ api/src/config/env/hotsalessever.js
@@ -0,0 +1,11 @@
+module.exports={
+    server_url: 'http://localhost:3000',
+    mongo: {
+        host: 'ds261253.mlab.com',
+        port: 61253,
+        user: "hotsalespos",
+        password: "Lehoangthienan123",
+        database: "hotsales",
+    }
+};
+  
