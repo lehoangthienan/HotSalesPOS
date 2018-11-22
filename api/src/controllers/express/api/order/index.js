e6b453c4674c61ab4a0b37dee1fff19e8f6adc2c
An Le <lehoangthienan@gmail.com>
2018-11-22 10:15:54 +0700

    Update logic to get orders by product id

--- api/src/controllers/express/api/order/index.js
+++ api/src/controllers/express/api/order/index.js
@@ -7,7 +7,8 @@ orderRoutes.get("/user/:user_id", require('./findall'))
 orderRoutes.post("/", require('./create'))
 orderRoutes.delete("/:order_id", require('./delete'))
 orderRoutes.put("/:order_id", require('./update'))
-orderRoutes.get("/:product_id", require('./find'))
+orderRoutes.get("/:order_id", require('./find'))
+orderRoutes.get("/product/:product_id", require('./findbyproduct'))
 
 
 module.exports = orderRoutes;
