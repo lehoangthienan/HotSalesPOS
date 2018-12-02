d06ab789020155881400e237d83ba7d13a6be1c3
An Le <lehoangthienan@gmail.com>
2018-12-02 21:11:11 +0700

    Add logic to scan qr code

--- kotlin/app/src/main/java/com/uit/daniel/hotsalesmanager/view/product/scanproduct/IntroScanActivity.kt
+++ kotlin/app/src/main/java/com/uit/daniel/hotsalesmanager/view/product/scanproduct/IntroScanActivity.kt
@@ -1,12 +1,37 @@
 package com.uit.daniel.hotsalesmanager.view.product.scanproduct
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
+import com.uit.daniel.hotsalesmanager.R
+import kotlinx.android.synthetic.main.activity_intro_scan.*
 
 class IntroScanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_scan)
+
+        initView()
+        addeEvents()
+    }
+
+    private fun addeEvents() {
+        btScan.setOnClickListener {
+            val intent = Intent(this, ScanProductActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+    }
+
+    private fun initView() {
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
     }
 }
