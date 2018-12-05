2ee2365b1f4296db27f572f9c0badaa11224f5f0
An Le <lehoangthienan@gmail.com>
2018-12-05 14:40:55 +0700

    -Add logic to upload image to sever when create product

--- kotlin/app/src/main/java/com/uit/daniel/hotsalesmanager/view/product/createproduct/CreateProductActivity.kt
+++ kotlin/app/src/main/java/com/uit/daniel/hotsalesmanager/view/product/createproduct/CreateProductActivity.kt
@@ -1,11 +1,18 @@
 package com.uit.daniel.hotsalesmanager.view.product.createproduct
 
+import android.Manifest
 import android.app.Dialog
-import android.support.v7.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
 import com.tbruyelle.rxpermissions2.RxPermissions
 import com.uit.daniel.hotsalesmanager.R
+import kotlinx.android.synthetic.main.dialog_permissin_read_write_storage.*
+import kotlinx.android.synthetic.main.dialog_permission_camera.*
 
 class CreateProductActivity : AppCompatActivity() {
 
@@ -18,18 +25,68 @@ class CreateProductActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_product)
 
+        initView()
         addControls()
         getPermission()
-        initView()
     }
 
     private fun addControls() {
-        
+        dlPermissionStorage = Dialog(this)
+        dlPermissionStorage.setContentView(R.layout.dialog_permissin_read_write_storage)
+        dlPermissionStorage.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialogPermissionCamera = Dialog(this)
+        dialogPermissionCamera.setContentView(R.layout.dialog_permission_camera)
+        dialogPermissionCamera.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    private fun getPermissionCamera() {
+        val permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (permissionCamera == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            dialogPermissionCamera.show()
+            dialogPermissionCamera.tvAccept.setOnClickListener {
+                dialogPermissionCamera.dismiss()
+                rxPermissionsCAMERA
+                    .request(Manifest.permission.CAMERA)
+                    .subscribe { granted ->
+                        if (granted) {
+                        } else {
+                        }
+                    }
+            }
+            dialogPermissionCamera.tvCancel.setOnClickListener {
+                dialogPermissionCamera.dismiss()
+            }
+        }
     }
 
     private fun getPermission() {
-
+        var permissionStorage = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (permissionStorage == PackageManager.PERMISSION_GRANTED) {
+            getPermissionCamera()
+        } else {
+            dlPermissionStorage.show()
+            dlPermissionStorage.tvAcceptPermissionStorage?.setOnClickListener {
+                dlPermissionStorage.dismiss()
+                rxPermissionsSTORAGE
+                    .request(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .subscribe { granted ->
+                        if (granted) {
+                        } else {
+                            permissionStorage =
+                                    ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }
+                        getPermissionCamera()
+                    }
+            }
+            dlPermissionStorage.tvCancelPermissionStorage.setOnClickListener {
+                dlPermissionStorage.dismiss()
+                getPermissionCamera()
+            }
+        }
     }
+
 
     private fun initView() {
         val actionBar: ActionBar? = supportActionBar
--- kotlin/app/src/main/java/com/uit/daniel/hotsalesmanager/view/product/createproduct/CreateProductFragment.kt
+++ kotlin/app/src/main/java/com/uit/daniel/hotsalesmanager/view/product/createproduct/CreateProductFragment.kt
@@ -4,21 +4,38 @@ import android.annotation.SuppressLint
 import android.app.Fragment
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.design.widget.BottomSheetDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.uit.daniel.hotsalesmanager.R
-import com.uit.daniel.hotsalesmanager.utils.ToastSnackBar
-import com.uit.daniel.hotsalesmanager.utils.UserManagerUtil
+import com.uit.daniel.hotsalesmanager.utils.*
 import com.uit.daniel.hotsalesmanager.view.location.searchaddresslocation.SearchAddressLocationActivity
 import kotlinx.android.synthetic.main.fragment_create_product.*
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class CreateProductFragment : Fragment() {
 
     private lateinit var createGroupViewModel: CreateGroupViewModel
     private lateinit var userManagerUtil: UserManagerUtil
     private var imageUrl = ""
+    private lateinit var bottomSheetDialogUploadImage: BottomSheetDialog
+    private val intentUtils = IntentUtils()
+    var storage = FirebaseStorage.getInstance()
+    var storageRef = storage.getReferenceFromUrl("gs://hotsalesmanager-fef89.appspot.com")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_create_product, container, false)
@@ -27,7 +44,20 @@ class CreateProductFragment : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        addControls()
         addEvents()
+    }
+
+    private fun initView() {
+        if (progressBarAddLocation != null) progressBarAddLocation.visibility =
+                getVisibilityView(false)
+    }
+
+    private fun addControls() {
+        bottomSheetDialogUploadImage = BottomSheetDialog(activity)
+        bottomSheetDialogUploadImage.setContentView(R.layout.bottomsheetdialog_update_avatar_profile)
+        bottomSheetDialogUploadImage.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 
     private fun addEvents() {
@@ -46,8 +76,42 @@ class CreateProductFragment : Fragment() {
         }
     }
 
-    private fun showBottomDialog() {
+    private fun startCameraIntent() {
+        val takePictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (takePictureIntent.resolveActivity(activity.packageManager) != null) {
+            startCameraActivity()
+        }
+    }
 
+    private fun startCameraActivity() {
+        try {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePictureIntent.resolveActivity(activity.packageManager) != null) {
+                this.startActivityForResult(takePictureIntent, Constant.REQUEST_CAMERA)
+            }
+        } catch (e: Exception) {
+        }
+    }
+
+    private fun showBottomDialog() {
+        showBottomSheetDialogUploadImage()
+    }
+
+    private fun showBottomSheetDialogUploadImage() {
+        bottomSheetDialogUploadImage.show()
+        bottomSheetDialogUploadImage.findViewById<RelativeLayout>(R.id.takePhotoFromCamera)?.setOnClickListener {
+            try {
+                startCameraIntent()
+            } catch (e: Exception) {
+            }
+        }
+
+        bottomSheetDialogUploadImage.findViewById<RelativeLayout>(R.id.takePhotoFromLibrary)?.setOnClickListener {
+            try {
+                startActivityForResult(intentUtils.intentActionPick(), Constant.REQUEST_GALLERY)
+            } catch (e: Exception) {
+            }
+        }
     }
 
     private fun startSearchLocationActivity() {
@@ -106,6 +170,96 @@ class CreateProductFragment : Fragment() {
         super.onAttach(context)
         createGroupViewModel = CreateGroupViewModel(context)
         userManagerUtil = UserManagerUtil.getInstance(context)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (progressBarAddLocation != null) progressBarAddLocation.visibility =
+                getVisibilityView(true)
+
+        when (requestCode) {
+            Constant.REQUEST_GALLERY -> {
+                setImageForGallery(data)
+            }
+            Constant.REQUEST_CAMERA -> {
+                setImageForCamera(data)
+            }
+        }
+    }
+
+    private fun setImageForCamera(data: Intent?) {
+        if (data?.extras?.get("data") == null) return
+        val bitmap = data.extras?.get("data") as Bitmap
+
+        updateImageCameraToSever(bitmap)
+
+        Glide.with(this)
+            .asBitmap()
+            .load(bitmap)
+            .into(ivProductImage)
+        bottomSheetDialogUploadImage.dismiss()
+    }
+
+    private fun updateImageCameraToSever(bitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val childRef = storageRef.child("images/" + UUID.randomUUID().toString())
+        val uploadTask = childRef.putBytes(data)
+        uploadTask.addOnSuccessListener(object : OnSuccessListener<UploadTask.TaskSnapshot> {
+            override fun onSuccess(taskSnapshot: UploadTask.TaskSnapshot?) {
+                childRef.downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri> {
+                    @SuppressLint("CheckResult")
+                    override fun onSuccess(uri: Uri?) {
+                        imageUrl = uri.toString()
+
+                        if (progressBarAddLocation != null) progressBarAddLocation.visibility =
+                                getVisibilityView(false)
+                    }
+                })
+            }
+        })
+    }
+
+    private fun setImageForGallery(data: Intent?) {
+        //Get uri from picture choose from gallery
+        val uri = data?.data ?: return
+        val projection = arrayOf(android.provider.MediaStore.Images.Media.DATA)
+
+        val cursor = activity.contentResolver.query(uri, projection, null, null, null)
+        cursor.moveToFirst()
+
+        val columnIndex = cursor.getColumnIndex(projection[0])
+        val filepath = cursor.getString(columnIndex)
+        cursor.close()
+
+        val bitmap = BitmapFactory.decodeFile(filepath)
+        ivProductImage.setImageBitmap(bitmap)
+        bottomSheetDialogUploadImage.dismiss()
+
+        updateImageToSever(uri)
+    }
+
+    private fun updateImageToSever(uri: Uri) {
+        if (uri != null) {
+            val childRef = storageRef.child("images/" + UUID.randomUUID().toString())
+            val uploadTask = childRef.putFile(uri)
+            uploadTask.addOnSuccessListener(object : OnSuccessListener<UploadTask.TaskSnapshot> {
+                override fun onSuccess(taskSnapshot: UploadTask.TaskSnapshot?) {
+                    childRef.downloadUrl.addOnSuccessListener(object : OnSuccessListener<Uri> {
+                        @SuppressLint("CheckResult")
+                        override fun onSuccess(uri: Uri?) {
+                            imageUrl = uri.toString()
+
+                            if (progressBarAddLocation != null) progressBarAddLocation.visibility =
+                                    getVisibilityView(false)
+                        }
+                    })
+                }
+            })
+        }
     }
 
 }
